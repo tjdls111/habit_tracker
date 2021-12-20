34e2e9afda53bb52cf9741451afcb1245397f4b7
tjdls111 <tjdls111@naver.com>
2021-12-20 20:57:37 +0900

    habit crd

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import './app.css';
+import Habits from './components/habits';
 
 function App() {
-  const name = 'seoin';
   return (
-    <React.Fragment>
-      <h1 className='title' onClick="">hello {name} ! </h1>
-      <h1>Bye!</h1>
-      {['😀','🌲'].map(item =>(
-          <h1>{item}</h1>))}
-    </React.Fragment>
+      <Habits></Habits>
     )
 }
 
---
+++ src/components/habits.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Habit from './habit';
+
+class Habits extends Component {
+  state ={
+    habits:[
+      {id:1,name:'Reading', count:0},
+      {id:2,name:'Coding', count:0},
+      {id:3,name:'Running', count:0}
+    ],
+  };
+
+  
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count ++;
+    this.setState({habits});
+
+  };
+  
+  handleDecrement = (habit) =>{
+    // console.log({habit});
+    // this.habits[habit.id] -= 1;
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count = habits[index].count-1 < 0 ? 0: habits[index].count-1;
+    this.setState({habits});
+    
+    
+  };
+  
+  handleDelete = (habit)=>{
+    // console.log({habit});
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits.splice(index,1);
+    this.setState({habits});
+  };
+
+  render() {
+    return (
+      <ul>
+        {
+          this.state.habits.map(habit=>(
+            <Habit key={habit.id} habit={habit} 
+              onIncrement={this.handleIncrement} 
+              onDecrement = {this.handleDecrement}
+              onDelete = {this.handleDelete}
+              />
+          ))
+        }
+        
+      </ul>
+    );
+  }
+}
+
+export default Habits;
