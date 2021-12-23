95085268cd7784d531db3a18aa214e1a0565485f
tjdls111 <tjdls111@naver.com>
2021-12-23 14:47:01 +0900

    class->function

--- src/components/createForm.jsx
+++ src/components/createForm.jsx
@@ -1,40 +1,32 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 import '../app.css';
 
-class CreateForm extends PureComponent{
-  state = {
+const CreateForm = memo((props) => {
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
 
-  };
-
-  inputRef = React.createRef();
-
-  onSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault(); // 기본 기능 취소
     // console.log('submit!!');
-    const name = this.inputRef.current.value
-    name && this.props.onAdd(name); // 이름 받아왔으면, onAdd에 넘김
-    this.inputRef.current.value = '';
-
+    const name = inputRef.current.value;
+    name && props.onAdd(name); // 이름 받아왔으면, onAdd에 넘김
+    inputRef.current.value = '';
   }
 
-  render(){
-    return (
-        <form 
-          action={this.addTodo} 
-          onSubmit={this.onSubmit}
-          
-          >
-          <input 
-            ref = {this.inputRef} 
-            type="text" 
-            className="add-input" 
-            placeholder='만들고 싶은 습관을 입력하세요!'/>
-          <button className='input-btn'>추가하기</button>
-        </form>
-    )
-
-
-  };
-}
+  return (
+    <form 
+      ref = {formRef}
+      action={onSubmit} 
+      onSubmit={onSubmit}
+      >
+      <input 
+        ref = {inputRef} 
+        type="text" 
+        className="add-input" 
+        placeholder='만들고 싶은 습관을 입력하세요!'/>
+      <button className='input-btn'>추가하기</button>
+    </form>
+  )
+});
 
 export default CreateForm;
