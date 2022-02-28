f70f4798e5b89bd50ad1d9da73cbb3d3ccd7d209
Seoin <68271159+tjdls111@users.noreply.github.com>
2022-02-28 17:56:29 +0900

    refactoring

--- src/app.jsx
+++ src/app.jsx
@@ -4,56 +4,27 @@ import Habits from "./components/habits";
 import Navbar from "./components/navbar";
 import ResetBtn from "./components/resetBtn";
 
-const App = () => {
-  const [habits, setHabits] = useState([
-    { id: 1, name: "Reading", count: 0 },
-    { id: 2, name: "Coding", count: 0 },
-    { id: 3, name: "Running", count: 0 },
-  ]);
+const App = ({ presenter }) => {
+  const [habits, setHabits] = useState(presenter.getHabits());
 
   const handleIncrement = (habit) => {
-    const habits2 = habits.map((item) => {
-      if (item.id === habit.id) {
-        return { ...habit, count: habit.count + 1 };
-      }
-      return item;
-    });
-    setHabits(habits2);
+    presenter.increment(habit, setHabits);
   };
 
   const handleDecrement = (habit) => {
-    const habits2 = habits.map((item) => {
-      if (item.id === habit.id) {
-        return { ...item, count: item.count <= 0 ? 0 : item.count - 1 };
-      }
-      return item;
-    });
-    setHabits(habits2);
+    presenter.decrement(habit, setHabits);
   };
 
   const handleDelete = (habit) => {
-    // console.log({habit});
-    const habits2 = [...habits];
-    const index = habits.indexOf(habit);
-    habits2.splice(index, 1);
-    setHabits(habits2);
+    presenter.delete(habit, setHabits);
   };
 
   const handleAdd = (name) => {
-    const habit = { id: Date.now(), name: name, count: 0 };
-    const habits2 = [...habits, habit];
-    setHabits(habits2);
+    presenter.add(name, setHabits);
   };
 
   const handleReset = () => {
-    const habits2 = habits.map((item) => {
-      if (item.count !== 0) {
-        return { ...item, count: 0 };
-      }
-      return item;
-    });
-
-    setHabits(habits2);
+    presenter.reset(setHabits);
   };
 
   return (
--- src/habit_presenter.js
+++ src/habit_presenter.js
@@ -8,12 +8,44 @@ export default class HabitPresenter {
   }
 
   increment(habit, update) {
-    const habits = this.state.habits.map((item) => {
+    this.habits = this.habits.map((item) => {
       if (item.id === habit.id) {
         return { ...habit, count: habit.count + 1 };
       }
       return item;
     });
+    update(this.habits);
+  }
+
+  decrement(habit, update) {
+    this.habits = this.habits.map((item) => {
+      if (item.id === habit.id) {
+        return { ...item, count: item.count <= 0 ? 0 : item.count - 1 };
+      }
+      return item;
+    });
+    update(this.habits);
+  }
+  delete(habit, update) {
+    const index = this.habits.indexOf(habit);
+    this.habits.splice(index, 1);
+    update(this.habits);
+  }
+
+  add(name, update) {
+    const habit = { id: Date.now(), name: name, count: 0 };
+    this.habits = [...this.habits, habit];
+    update(this.habits);
+  }
+
+  reset(update) {
+    this.habits = this.habits.map((item) => {
+      if (item.count !== 0) {
+        return { ...item, count: 0 };
+      }
+      return item;
+    });
+
     update(this.habits);
   }
 }
